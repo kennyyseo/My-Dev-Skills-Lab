19419a503adecc6b15f91a93cf61c90e0ccd3c33
Kenny Seo <kennyyseo@gmail.com>
2020-08-25 12:43:02 -0700

    Completed Lab

--- express-dev-skills/routes/index.js
+++ express-dev-skills/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Skill List" });
+  res.redirect("/skills");
 });
 
 module.exports = router;
